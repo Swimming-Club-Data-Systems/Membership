264caf1dbc2a1ab62129965995440c5211e5ab38
Chris Heppell <clheppell@icloud.com>
2022-04-01 01:23:22 +0100

    Start custom date picker work

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -110,6 +110,10 @@ module.exports = (env, options) => {
           }
         },
         {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
           test: /\.(scss)$/,
           use: [
             MiniCssExtractPlugin.loader,
