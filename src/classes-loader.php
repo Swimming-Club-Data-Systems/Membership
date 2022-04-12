fb19921365490031bed0cc11becefe8307aea6d4
Chris Heppell <clheppell@icloud.com>
2022-04-12 18:35:54 +0100

    Tenancy Work

--- src/classes-loader.php
+++ src/classes-loader.php
@@ -10,4 +10,15 @@ function loadObjects($className)
   }
 }
 
+function loadLaravelModelObjects($className)
+{
+  $path = BASE_PATH . 'v2/';
+  $className = lcfirst(str_replace("\\", DIRECTORY_SEPARATOR, $className));
+  $filename = $path . $className . '.php';
+  if (file_exists($filename)) {
+    require_once $filename;
+  }
+}
+
 spl_autoload_register('loadObjects');
+spl_autoload_register('loadLaravelModelObjects');
