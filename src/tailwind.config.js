d305a9fd2ab6e8c485c5ca69abe862354390476a
Chris Heppell <clheppell@icloud.com>
2022-07-06 10:17:31 +0100

    Fix Inter config

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,5 +1,7 @@
 // /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./js/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
