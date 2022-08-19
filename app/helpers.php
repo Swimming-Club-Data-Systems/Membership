9f974d299fcaeba2aa1c27a52b3901163d72a48c
Chris Heppell <clheppell@icloud.com>
2022-08-19 20:57:27 +0100

    Fix filesystem asset helper

--- app/helpers.php
+++ app/helpers.php
@@ -4,10 +4,10 @@ function getUploadedAssetUrl($asset)
 {
   if (substr($asset, 0, 5) === "X-S3:") {
     $asset = ltrim(substr($asset, 5), '/');
-    if (env('AWS_CLOUDFRONT_ROOT')) {
-      return env('AWS_CLOUDFRONT_ROOT') . $asset;
-    } else if (env('AWS_S3_REGION') && env('AWS_S3_BUCKET')) {
-      return 'https://' . env('AWS_S3_BUCKET') . '.s3.' . env('AWS_S3_REGION') . '.amazonaws.com/' . $asset;
+    if (config('filesystems.disks.s3.cloudfront_url')) {
+      return config('filesystems.disks.s3.cloudfront_url') . $asset;
+    } else if (config('filesystems.disks.s3.region') && config('filesystems.disks.s3.bucket')) {
+      return 'https://' . config('filesystems.disks.s3.bucket') . '.s3.' . config('filesystems.disks.s3.region') . '.amazonaws.com/' . $asset;
     }
   }
 
