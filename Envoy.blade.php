c02b5c8b5a0da364a027399dfeceae6d039f93e1
Chris Heppell <chris.heppell@myswimmingclub.uk>
2022-08-23 19:05:52 +0100

    Add branch to deployment

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -23,7 +23,7 @@
 @task('clone_repository')
     echo 'Cloning repository'
     [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
-    git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
+    git clone --depth 1 -b {{ $branch }} {{ $repository }} {{ $new_release_dir }}
     cd {{ $new_release_dir }}
     git reset --hard {{ $commit }}
 @endtask
