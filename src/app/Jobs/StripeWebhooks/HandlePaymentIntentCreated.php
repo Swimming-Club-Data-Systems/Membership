786ebf45aec4fd8a46bebda5df5855df8eb081ee
Chris Heppell <chris.heppell@myswimmingclub.uk>
2023-02-12 18:45:02 +0000

    Create Stripe Payment Intent webhook handler stubs

---
+++ src/app/Jobs/StripeWebhooks/HandlePaymentIntentCreated.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs\StripeWebhooks;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Spatie\WebhookClient\Models\WebhookCall;
+
+class HandlePaymentIntentCreated implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public WebhookCall $webhookCall
+    )
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
