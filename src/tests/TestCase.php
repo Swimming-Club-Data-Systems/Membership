2732e0c5ebd063afc09f94049ed1569d82736f8d
Chris Heppell <chris.heppell@myswimmingclub.uk>
2023-11-20 13:29:22 +0000

    Prepare for tenant feature testing

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Models\Central\Tenant;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -12,4 +13,22 @@ abstract class TestCase extends BaseTestCase
      * Indicates whether the default seeder should run before each test.
      */
     protected bool $seed = true;
+
+    protected bool $tenancy = false;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if ($this->tenancy) {
+            $this->initializeTenancy();
+        }
+    }
+
+    public function initializeTenancy(): void
+    {
+        $tenant = Tenant::create();
+
+        tenancy()->initialize($tenant);
+    }
 }
