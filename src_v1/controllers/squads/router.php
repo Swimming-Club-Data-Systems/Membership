f0c4800c74d97ed006a21d3f6706c7c93a0ab906
Chris Heppell <chris.heppell@myswimmingclub.uk>
2023-11-21 23:04:17 +0000

    Fix old squads require for moves router

--- src_v1/controllers/squads/router.php
+++ src_v1/controllers/squads/router.php
@@ -2,9 +2,3 @@
 
 $userID = $_SESSION['TENANT-' . app()->tenant->getId()]['UserID'];
 $access = $_SESSION['TENANT-' . app()->tenant->getId()]['AccessLevel'];
-
-if ($access == "Admin" || $access == "Coach") {
-	$this->group('/moves', function () {
-		require 'moves/router.php';
-	});
-}
