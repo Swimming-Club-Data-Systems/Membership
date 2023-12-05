93189bd9f3e00460d4827e7e34682fb6d78e7e0c
Chris Heppell <chris.heppell@myswimmingclub.uk>
2023-12-05 23:40:01 +0000

    Update v1 app links to membership renewal pages

--- src_v1/controllers/memberships/home.php
+++ src_v1/controllers/memberships/home.php
@@ -158,7 +158,7 @@ include BASE_PATH . "views/header.php";
 						</p>
 					</div>
 					<p class="mb-0 mt-auto d-flex">
-						<a href="<?= htmlspecialchars(autoUrl('memberships/renewal')) ?>" class="btn btn-primary">
+						<a href="/renewal" class="btn btn-primary">
 							Go
 						</a>
 					</p>
